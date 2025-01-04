c6ef7eae3a944dc8ccb4af5f51768a5aa569d3b6
Sathish <ssathish789@gmail.com>
2025-01-04 10:53:15 +0100

    feat: initial project setup

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.22"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
+}
+
+group = "co.in.acmesense"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("io.ktor:ktor-client-core:2.3.8")
+    implementation("io.ktor:ktor-client-cio:2.3.8")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.8")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3") // explicitly add coroutine dependency
+    implementation("ch.qos.logback:logback-classic:1.5.3")
+
+    testImplementation(kotlin("test"))
+    testImplementation("io.mockk:mockk:1.13.8")
+}
+
+
+kotlin {
+    jvmToolchain(17)
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+
+
