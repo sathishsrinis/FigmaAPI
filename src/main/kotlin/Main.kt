cb75dd55540193b92d2f2a42a73d3ebf175ba154
Sathish <ssathish789@gmail.com>
2025-01-04 11:09:27 +0100

    feat: Request and print the response from the figma apis in the main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
-
+import co.`in`.acmesense.FigmaService
+import kotlinx.coroutines.runBlocking
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.FileInputStream
@@ -14,7 +15,12 @@ fun main() {
         .split(",")
         .map { it.trim() }
 
+    val figmaService = FigmaService(figmaAccessToken)
 
+    runBlocking {
+        fetchAndLogNodes(figmaService, fileKey, nodeIds, logger)
+        fetchAndLogFile(figmaService, fileKey, logger)
+    }
 }
 
 private fun loadProperties(path: String, logger: Logger): Properties? {
@@ -25,5 +31,24 @@ private fun loadProperties(path: String, logger: Logger): Properties? {
     } catch (e: Exception) {
         logger.error("Error loading properties file ${e.message}", e)
         null
+    }
+}
+
+private suspend fun fetchAndLogNodes(figmaService: FigmaService, fileKey: String, nodeIds: List<String>, logger: Logger) {
+    try {
+        val nodesJson = figmaService.getNodes(fileKey, nodeIds)
+        println("Response of Nodes : $nodesJson")
+    } catch (e: Exception) {
+        logger.error("Error fetching nodes ${e.message}", e)
+    }
+}
+
+
+private suspend fun fetchAndLogFile(figmaService: FigmaService, fileKey: String, logger: Logger) {
+    try {
+        val fileJson = figmaService.getFile(fileKey)
+        println("Response of file: $fileJson")
+    } catch (e: Exception) {
+        logger.error("Error fetching file: ${e.message}", e)
     }
 }
